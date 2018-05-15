6c7b2a0616f1097532a5a041e8a05e8080cdcea1
scott-mac-p9 <scozv@yandex.com>
2018-05-15 22:16:25 +0800

    fix the name issue for zip file

--- os/macOS.sh
+++ os/macOS.sh
@@ -15,9 +15,9 @@
 
   df_echo "download the environment scripts"
   mkdir -p ~/opt/.df
-  curl -o ~/opt/.df/dotfiles.tar.gz https://github.com/scozv/dotfiles/archive/feature/macOS.zip
+  curl -o ~/opt/.df/dotfiles.zip https://github.com/scozv/dotfiles/archive/feature/macOS.zip
   # https://apple.stackexchange.com/a/43119
-  unzip ~/opt/.df/dotfiles.tar.gz -d ~/opt/.df/
+  unzip ~/opt/.df/dotfiles.zip -d ~/opt/.df/
 
   DF_OS_PATH="~/opt/.df/dotfiles-feature-macOS/os/src"
 
